49c78715f92848280bf179f3a899b76941e11392
sisodiyaashraf <ashrafsisodiya478@gmail.com>
2026-09-04 09:35:38 +0530

    add unit tests for SensorUtils, PresenceDetector, and EchoProvider logic

--- test/presence_detector_test.dart
+++ test/presence_detector_test.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:whispers/core/sensor_utils.dart';
 import 'package:whispers/data/repositories/save_repository.dart';
@@ -9,8 +12,14 @@ import 'package:whispers/presentation/providers/echo_provider.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
+  const sensorChannel = MethodChannel('dev.fluttercommunity.plus/sensors/method');
+
   setUp(() {
     SharedPreferences.setMockInitialValues({});
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(sensorChannel, (MethodCall methodCall) async {
+      return null;
+    });
   });
 
   group('SensorUtils tests', () {
@@ -28,7 +37,8 @@ void main() {
 
   group('PresenceDetector & EchoProvider tests', () {
     test('registerTouch updates PresenceDetector signal', () async {
-      final detector = PresenceDetector();
+      final sensorController = StreamController<AccelerometerEvent>();
+      final detector = PresenceDetector(sensorStream: sensorController.stream);
       detector.start();
 
       expect(detector.currentSignal, PresenceSignal.idle);
@@ -36,11 +46,18 @@ void main() {
       detector.registerTouch();
       expect(detector.currentSignal, PresenceSignal.activelyTouching);
 
+      sensorController.add(AccelerometerEvent(3.0, 3.0, 12.0, DateTime.now()));
+      await Future.delayed(Duration.zero);
+
+      expect(detector.currentSignal, PresenceSignal.pickedUp);
+
       detector.dispose();
+      await sensorController.close();
     });
 
     test('EchoProvider receives signals from PresenceDetector end-to-end', () async {
-      final detector = PresenceDetector();
+      final sensorController = StreamController<AccelerometerEvent>();
+      final detector = PresenceDetector(sensorStream: sensorController.stream);
       final repository = SaveRepository();
       final provider = EchoProvider(
         presenceDetector: detector,
@@ -60,6 +77,7 @@ void main() {
       expect(notifyCount, greaterThan(0));
 
       await provider.endSession();
+      await sensorController.close();
     });
   });
 }
