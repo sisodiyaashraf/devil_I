2a98986c1ec74b54a81b95d942555eba1b86c91b
sisodiyaashraf <ashrafsisodiya478@gmail.com>
2026-09-05 19:10:39 +0530

    add CorruptedText widget with dynamic text flickering animation

--- lib/presentation/widgets/corrupted_text.dart
+++ lib/presentation/widgets/corrupted_text.dart
@@ -22,7 +22,7 @@ class CorruptedText extends StatefulWidget {
 
 class _CorruptedTextState extends State<CorruptedText> {
   static const List<String> _glitchChars = [
-    '░', '▒', '▓', '█', '§', 'µ', 'Ø', '▲', '?', '#', '&', '$', '⍉'
+    '░', '▒', '▓', '█', '§', 'µ', 'Ø', '▲', '?', '#', '&', r'$', 'X'
   ];
 
   late String _displayedText;
