1ec2ab7fe322d9d37a89c58f8629a9f62dd95555
sisodiyaashraf <ashrafsisodiya478@gmail.com>
2026-09-06 19:11:22 +0530

    add SessionMemory entity to manage session state and metadata

--- lib/domain/entities/session_memory.dart
+++ lib/domain/entities/session_memory.dart
@@ -3,11 +3,14 @@ class SessionMemory {
   final int sessionCount;
   final int peakCorruption;
   final String? userLabel;
+  final Map<String, String> userAnswers;
 
   const SessionMemory({
     required this.lastOpenedAt,
     required this.sessionCount,
     required this.peakCorruption,
     this.userLabel,
+    this.userAnswers = const {},
   });
 }
+
