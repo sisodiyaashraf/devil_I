2614991f915fd28b336963df5b1abac2279a2685
sisodiyaashraf <ashrafsisodiya478@gmail.com>
2026-09-07 07:51:04 +0530

    implement PresenceDetector use case to track user motion and idle state via accelerometer

--- lib/domain/usecases/presence_detector.dart
+++ lib/domain/usecases/presence_detector.dart
@@ -5,21 +5,23 @@ import '../../core/sensor_utils.dart';
 import '../entities/presence_signal.dart';
 
 class PresenceDetector {
-  final StreamController<PresenceSignal> _signalController =
-      StreamController<PresenceSignal>.broadcast();
+  final StreamController<(PresenceSignal, double)> _signalController =
+      StreamController<(PresenceSignal, double)>.broadcast();
   final Stream<AccelerometerEvent>? _customSensorStream;
   StreamSubscription<AccelerometerEvent>? _accelSubscription;
   Timer? _idleCheckTimer;
   DateTime _lastTouch = DateTime.now();
   DateTime? _lastMotionSignalTime;
   PresenceSignal _currentSignal = PresenceSignal.idle;
+  double _lastRawX = 0.0;
   bool _isPaused = false;
 
   PresenceDetector({Stream<AccelerometerEvent>? sensorStream})
       : _customSensorStream = sensorStream;
 
-  Stream<PresenceSignal> get signalStream => _signalController.stream;
+  Stream<(PresenceSignal, double)> get signalStream => _signalController.stream;
   PresenceSignal get currentSignal => _currentSignal;
+  double get lastRawX => _lastRawX;
 
   void start() {
     _isPaused = false;
@@ -63,10 +65,11 @@ class PresenceDetector {
   void registerTouch() {
     if (_isPaused) return;
     _lastTouch = DateTime.now();
-    _emitSignal(PresenceSignal.activelyTouching);
+    _emitSignal(PresenceSignal.activelyTouching, _lastRawX);
   }
 
   void _onAccelerometerEvent(double x, double y, double z) {
+    _lastRawX = x;
     final now = DateTime.now();
     if (_lastMotionSignalTime != null &&
         now.difference(_lastMotionSignalTime!) < const Duration(milliseconds: 1200)) {
@@ -75,10 +78,10 @@ class PresenceDetector {
 
     if (SensorUtils.isPickupMotion(x, y, z)) {
       _lastMotionSignalTime = now;
-      _emitSignal(PresenceSignal.pickedUp);
+      _emitSignal(PresenceSignal.pickedUp, x);
     } else if (SensorUtils.isSignificantTilt(x, y, z)) {
       _lastMotionSignalTime = now;
-      _emitSignal(PresenceSignal.tilted);
+      _emitSignal(PresenceSignal.tilted, x);
     }
   }
 
@@ -86,15 +89,15 @@ class PresenceDetector {
     if (_isPaused) return;
     final secondsSinceTouch = DateTime.now().difference(_lastTouch).inSeconds;
     if (secondsSinceTouch >= AppConstants.idleTriggerSeconds) {
-      _emitSignal(PresenceSignal.idle);
+      _emitSignal(PresenceSignal.idle, _lastRawX);
     }
   }
 
-  void _emitSignal(PresenceSignal signal) {
+  void _emitSignal(PresenceSignal signal, [double? rawX]) {
     if (_signalController.isClosed || _isPaused) return;
     if (_currentSignal != signal) {
       _currentSignal = signal;
-      _signalController.add(signal);
+      _signalController.add((signal, rawX ?? _lastRawX));
     }
   }
 
