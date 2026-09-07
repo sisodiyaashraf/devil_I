45ad0985814aaba1598311046ebdcc0e1b60beb4
sisodiyaashraf <ashrafsisodiya478@gmail.com>
2026-09-07 08:00:22 +0530

    test: add responsive UI tests for MainScreen across multiple device sizes

--- test/responsive_screen_test.dart
+++ test/responsive_screen_test.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:whispers/main.dart';
+import 'package:whispers/presentation/providers/echo_provider.dart';
+import 'package:whispers/presentation/screens/main_screen.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
