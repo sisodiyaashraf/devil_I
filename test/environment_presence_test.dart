eaed4b122e46f784b3cf6c6421a6d57a48313bbf
sisodiyaashraf <ashrafsisodiya478@gmail.com>
2026-09-07 08:17:13 +0530

    test: add unit and integration tests for environment services, entities, and providers

--- test/environment_presence_test.dart
+++ test/environment_presence_test.dart
@@ -43,8 +43,18 @@ class MockEnvironmentService extends EnvironmentService {
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
+  const sensorChannel = MethodChannel('dev.fluttercommunity.plus/sensors/method');
+  const globalAudioChannel = MethodChannel('xyz.luan/audioplayers.global');
+  const playerAudioChannel = MethodChannel('xyz.luan/audioplayers');
+
   setUp(() {
     SharedPreferences.setMockInitialValues({});
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(sensorChannel, (MethodCall methodCall) async => null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(globalAudioChannel, (MethodCall methodCall) async => null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(playerAudioChannel, (MethodCall methodCall) async => null);
   });
 
   group('EnvironmentService Tests', () {
